0c9a0e6794cd54e5efadc9d71a9c84524cff8cfd
Ranasinghe TKSA <sachin.ranasinghe1998.13@gmail.com>
2023-02-26 15:34:22 +0530

    Initial Commit

---
+++ CustomerManagementPublisher/src/CustomerManagementPublisher/SimpleLogServiceImpl.java
@@ -0,0 +1,13 @@
+package customermanagementpublisher;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class SimpleLogServiceImpl implements SimpleLogService {
+
+	@Override
+	public void log(String message) {
+		System.out.println(message);
+	}
+	
+}
